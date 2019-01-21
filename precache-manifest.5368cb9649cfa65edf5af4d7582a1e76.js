0d7665ed503fbfc03e5c3b10b3e7aec7a4d90f61
alpcanaydin <alpcan@alpcanaydin.com>
2019-01-21 12:15:54 +0300

    Updates

---
+++ precache-manifest.5368cb9649cfa65edf5af4d7582a1e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc40968d7d8ba23b5b24",
+    "url": "/say-it/static/css/main.15784cab.chunk.css"
+  },
+  {
+    "revision": "fc40968d7d8ba23b5b24",
+    "url": "/say-it/static/js/main.fc40968d.chunk.js"
+  },
+  {
+    "revision": "bda61fa9879b71e58397",
+    "url": "/say-it/static/js/1.bda61fa9.chunk.js"
+  },
+  {
+    "revision": "75798ee4cfcdecf718d2",
+    "url": "/say-it/static/js/runtime~main.75798ee4.js"
+  },
+  {
+    "revision": "1115a09423204c7f080ed632c32c3449",
+    "url": "/say-it/index.html"
+  }
+];
